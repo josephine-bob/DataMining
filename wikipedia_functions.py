7ab0f448f0071fcaef2e7b66e23bba889b5ecf84
josephine-bob <josephine.robert@eleves.ec-nantes.fr>
2014-11-27 15:27:44 +0100

    wikipedia_functions flake8 ok

--- wikipedia_functions.py
+++ wikipedia_functions.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Useful function to extract words and calculate the sentiment of a page thanks to AFINN-111 dictionary
+Useful function to extract words and calculate the sentiment of a page
+use AFINN-111 dictionary
 """
 
 import wikipedia
@@ -9,36 +10,41 @@ from nltk import word_tokenize, WordNetLemmatizer, corpus
 stopwords = corpus.stopwords.words('english')
 
 average_sentiment = []
-companies=[]
+companies = []
 
 
 def open_dictionary():
     """
-    Put words and their sentiment from the AFINN-111 word list into a dictionary
+    Put words and their sentiment into a dictionary
+    From the AFINN-111 word list
     """
-    words=[]
-    sentiment_words=[]
+    words = []
+    sentiment_words = []
     with open('AFINN/AFINN-111.txt', 'r') as in_file:
         for line in in_file.readlines():
             # word and values are separated by a tab
-            words.append(line.split('\t')[0]) 
+            words.append(line.split('\t')[0])
             sentiment_words.append(line.split('\t')[1].split('\n')[0])
-            
+
     return dict(zip(words, sentiment_words))
-       
+
 
 def normalize_text(text):
     """
     Normalize the text
     tokenize, remove stopwords, and lemmatize
     """
-    #put the text in words
+    # put the text in words
     tokenized_text = word_tokenize(text)
-    #remove stopwords
-    no_stopwords_text= [w for w in tokenized_text if w.lower() not in stopwords]
-    #lemmatize    
-    lemmatized_text = [WordNetLemmatizer().lemmatize(t) for t in no_stopwords_text]
-    
+    # remove stopwords
+    no_stopwords_text = [
+        w for w in tokenized_text if w.lower() not in stopwords
+    ]
+    # lemmatize
+    lemmatized_text = [
+        WordNetLemmatizer().lemmatize(t) for t in no_stopwords_text
+    ]
+
     return lemmatized_text
 
 
@@ -50,48 +56,51 @@ def sentiment(words, sentiment_dictionary):
     sentiment_value = 0
     average_sentiment = 0
     for word in words:
-        for key in sentiment_dictionary: # check if word appears in the dictionary
+        # check if word appears in the dictionary
+        for key in sentiment_dictionary:
             # count the number of words and sum their sentiment
-            if word.lower() == key:   
+            if word.lower() == key:
                 count += 1
                 sentiment_value += float(sentiment_dictionary[key])
             if count > 0:
-                #if there is at least one word, calculate the average dividing the sum by the number of words
+                # if there is at least one word,
+                # calculate the average sentiment
+                # dividing the sum by the number of words
                 average_sentiment = sentiment_value/count
-                
+
     return average_sentiment
+
 
 def company_sentiment(company):
     """
-    Evaluate the average sentiment of a company from its wikipedia page 
+    Evaluate the average sentiment of a company from its wikipedia page
     """
-    #load the page and extract content
+    # load the page and extract content
     page = wikipedia.page(company)
     text = page.content
 
-    #load the dictionary
+    # load the dictionary
     sentiment_dictionary = open_dictionary()
-    
-    #normalize the text
+
+    # normalize the text
     normalized_text = normalize_text(text)
-    
-    #calculate the average sentiment
-    average_sentiment.append(sentiment(normalized_text,sentiment_dictionary))              
-    
-    #stock the companies
+
+    # calculate the average sentiment
+    average_sentiment.append(sentiment(normalized_text, sentiment_dictionary))
+
+    # stock the companies
     companies.append(company)
-	
+
     return average_sentiment
 
 
 def reinitialize():
     """
-    Put the data collected to empty list, if you want to start another comparison
+    Put the data collected to empty list,
+    if you want to start another comparison
     """
     global average_sentiment
     average_sentiment = []
-	
+
     global companies
     companies = []
-    
-    
